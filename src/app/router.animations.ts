d326aecd97c517d84e7552756d0807b15a5fa3c2
Allan Demarbre <demarbreallan@gmail.com>
2018-11-17 18:25:17 +0100

    Fixing build errors

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -4,7 +4,7 @@ export function routerTransition() {
   return slideToLeft();
 }
 
-function slideToLeft() {
+export function slideToLeft() {
   return trigger('routerTransition', [
     state('void', style({position:'fixed', width:'100%'}) ),
     state('*', style({position:'fixed', width:'100%'}) ),
